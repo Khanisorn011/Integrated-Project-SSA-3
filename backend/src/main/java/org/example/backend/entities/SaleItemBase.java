12c1572255dd040cab92b9a070bc94f402e9df74
Wittawin1 <winthemath16@gmail.com>
2025-05-04 12:52:01 +0700

    079 - [update] fix api path /sale/items

--- backend/src/main/java/org/example/backend/entities/SaleItemBase.java
+++ backend/src/main/java/org/example/backend/entities/SaleItemBase.java
@@ -50,7 +50,7 @@ public class SaleItemBase {
     @Column(name = "storageGb")
     private Integer storageGb;
 
-    @Column(name = "screenSizeInch", precision = 10, scale = 1)
+    @Column(name = "screenSizeInch", precision = 4, scale = 1)
     private BigDecimal screenSizeInch;
 
     @Lob
